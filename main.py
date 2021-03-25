4ff3cdb79a805a59aeda215c2ab5c4d4263a8cca
maraIex <64632154+maraIex@users.noreply.github.com>
2021-03-25 11:45:18 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from data import db_session
 from data.news import News
 from data.users import User
 from forms.user import RegisterForm
-import datetime
+import os
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
-
-def main():
-    db_session.global_init("db/blogs.db")
-    app.run()
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+#
+#
+# def main():
+#     db_session.global_init("db/blogs.db")
+#     app.run()
     # user = User()
     # user.name = "Пользователь 1"
     # user.about = "биография пользователя 1"
@@ -54,38 +54,55 @@ def main():
     # for news in user.news:
     #     print(news)
 
+#
+# @app.route("/")
+# def index():
+#     db_sess = db_session.create_session()
+#     news = db_sess.query(News).filter(News.is_private != True)
+#     return render_template("index.html", news=news)
+#
+#
+# @app.route('/register', methods=['GET', 'POST'])
+# def reqister():
+#     form = RegisterForm()
+#     if form.validate_on_submit():
+#         if form.password.data != form.password_again.data:
+#             return render_template('register.html', title='Регистрация',
+#                                    form=form,
+#                                    message="Пароли не совпадают")
+#         db_sess = db_session.create_session()
+#         if db_sess.query(User).filter(User.email == form.email.data).first():
+#             return render_template('register.html', title='Регистрация',
+#                                    form=form,
+#                                    message="Такой пользователь уже есть")
+#         user = User(
+#             name=form.name.data,
+#             email=form.email.data,
+#             about=form.about.data
+#         )
+#         user.set_password(form.password.data)
+#         db_sess.add(user)
+#         db_sess.commit()
+#         return redirect('/login')
+#     return render_template('register.html', title='Регистрация', form=form)
+#
+#
+# if __name__ == '__main__':
+#     app.run()
+
+
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    db_sess = db_session.create_session()
-    news = db_sess.query(News).filter(News.is_private != True)
-    return render_template("index.html", news=news)
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        user = User(
-            name=form.name.data,
-            email=form.email.data,
-            about=form.about.data
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
+    return "Привет от приложения Flask"
 
 
 if __name__ == '__main__':
-    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
